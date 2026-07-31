956162f7124a5825d668d886d9cbfdf1df832b77
Ayush Pal <ayushpal.dev.in@gmail.com>
2026-07-31 20:28:17 +0530

    Create 1574-MaximumProductOfTwoElementsInAnArray

---
+++ 1574-MaximumProductOfTwoElementsInAnArray/1574-MaximumProductOfTwoElementsInAnArray.java
@@ -0,0 +1,17 @@
+// Last updated: 7/31/2026, 8:28:12 PM
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        for (int x : nums) {
+            if (x >= max1) {
+                max2 = max1;
+                max1 = x;
+            }
+            else if (x > max2) {
+                max2 = x;
+            }
+        }
+        return (max1-1)*(max2-1);
+    }
+}
